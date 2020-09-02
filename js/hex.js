0df39e5155ba5082b05244b89bee29439bb3632b
Brian Bleakley <brianbleakley@gmail.com>
2020-09-02 00:06:08 -0600

    fix icosahedron uv mapping

--- js/hex.js
+++ js/hex.js
@@ -6,13 +6,70 @@ const hexHeight = 3 * (triangleSize * 0.75 + 1);
 
 // https://hexagoncalculator.apphb.com/
 
+const getGlobeTriangles = () => {
+  let arcticTriangles = [];
+  for (let i = 0; i < 5; i++) {
+    let a = [6 + 12 * i, 0];
+    let b = [12 + 12 * i, 9];
+    let c = [0 + 12 * i, 9];
+    arcticTriangles.push([a, b, c]);
+  }
+  let cancerTriangles = [];
+  for (let i = 0; i < 5; i++) {
+    let a = [6 + 12 * i, 18];
+    let b = [12 + 12 * i, 9];
+    let c = [0 + 12 * i, 9];
+    cancerTriangles.push([b, a, c]);
+  }
+  let capricornTriangles = [];
+  for (let i = 0; i < 5; i++) {
+    let a = [12 + 12 * i, 9];
+    let b = [18 + 12 * i, 18];
+    let c = [6 + 12 * i, 18];
+    capricornTriangles.push([a, b, c]);
+  }
+  let antarcticTriangles = [];
+  for (let i = 0; i < 5; i++) {
+    let a = [12 + 12 * i, 27];
+    let b = [18 + 12 * i, 18];
+    let c = [6 + 12 * i, 18];
+    antarcticTriangles.push([b, a, c]);
+  }
+
+  let six = cancerTriangles[0];
+  let sixteen = antarcticTriangles[0];
+  let seventeen = antarcticTriangles[1];
+  let eighteen = antarcticTriangles[2];
+  let nineteen = antarcticTriangles[3];
+  let twenty = antarcticTriangles[4];
+
+  return [
+    ...arcticTriangles.reverse(),
+    [six[2], six[0], six[1]],
+    capricornTriangles[4],
+    cancerTriangles[4],
+    capricornTriangles[3],
+    cancerTriangles[3],
+    capricornTriangles[2],
+    cancerTriangles[2],
+    capricornTriangles[1],
+    cancerTriangles[1],
+    capricornTriangles[0],
+    [twenty[2], twenty[0], twenty[1]],
+    nineteen,
+    [sixteen[2], sixteen[0], sixteen[1]],
+    eighteen,
+    seventeen,
+  ];
+};
+
 export const drawHexTexture = (ctx, width, height) => {
   const Hex = Honeycomb.extendHex({
     size: 50, // default: 1
     orientation: "flat", // default: 'pointy'
   });
   const Grid = Honeycomb.defineGrid(Hex);
-  let grid = Grid.rectangle({ width: 66, height: 27 });
+  let grid = Grid.rectangle({ width: 67, height: 28 });
 
   let xOffset = 0;
   let yOffset = 0;
@@ -41,34 +98,102 @@ export const drawHexTexture = (ctx, width, height) => {
     ctx.stroke();
   });
 
-  let a = grid.get([0, 9]);
-  let b = grid.get([12, 9]);
-  let c = grid.get([6, 0]);
+  let triangles = getGlobeTriangles();
 
-  let ax = a.toPoint().add(a.center()).x;
-  let ay = a.toPoint().add(a.center()).y;
-  let bx = b.toPoint().add(b.center()).x;
-  let by = b.toPoint().add(b.center()).y;
-  let cx = c.toPoint().add(c.center()).x;
-  let cy = c.toPoint().add(c.center()).y;
+  triangles.forEach((t) => {
+    let a = grid.get(t[0]);
+    let b = grid.get(t[1]);
+    let c = grid.get(t[2]);
 
-  ctx.beginPath();
-  ctx.moveTo(ax, ay);
-  ctx.lineTo(bx, by);
-  ctx.lineTo(cx, cy);
-  ctx.lineTo(ax, ay);
-  ctx.strokeStyle = "green";
-  ctx.lineWidth = 5;
-  //ctx.stroke();
+    let ax = a.toPoint().add(a.center()).x;
+    let ay = a.toPoint().add(a.center()).y;
+    let bx = b.toPoint().add(b.center()).x;
+    let by = b.toPoint().add(b.center()).y;
+    let cx = c.toPoint().add(c.center()).x;
+    let cy = c.toPoint().add(c.center()).y;
 
-  return {
-    xStart: ax / width,
-    xEnd: bx / width,
-    yStart: (height - ay) / height,
-    yEnd: (height - cy) / height,
-    width,
-    height,
+    ctx.beginPath();
+    ctx.moveTo(ax, ay);
+    ctx.lineTo(bx, by);
+    ctx.lineTo(cx, cy);
+    ctx.lineTo(ax, ay);
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+  });
+
+  let drawLetter = (x, y, letter) => {
+    let hex = grid.get([x, y]);
+    let hx = hex.toPoint().add(hex.center()).x - 12;
+    let hy = hex.toPoint().add(hex.center()).y;
+    ctx.font = "48px monospace";
+    ctx.textBaseline = "middle";
+    ctx.strokeStyle = "white";
+    ctx.strokeText(letter, hx, hy);
   };
+
+  //drawLetter(6, 0, "a");
+  //drawLetter(12, 9, "b");
+  //drawLetter(0, 9, "c");
+
+  drawLetter(6, 6, "1");
+  drawLetter(18, 6, "2");
+  drawLetter(30, 6, "3");
+  drawLetter(42, 6, "4");
+  drawLetter(54, 6, "5");
+
+  drawLetter(6, 12, ".6");
+  drawLetter(12, 15, "7");
+  drawLetter(18, 12, "8");
+  drawLetter(24, 15, ".9");
+  drawLetter(30, 12, "10");
+  drawLetter(36, 15, "11");
+  drawLetter(42, 12, "12");
+  drawLetter(48, 15, "13");
+  drawLetter(54, 12, "14");
+  drawLetter(60, 15, "15");
+
+  drawLetter(12, 21, "16");
+  drawLetter(24, 21, "17");
+  drawLetter(36, 21, "18");
+  drawLetter(48, 21, "19");
+  drawLetter(60, 21, "20");
+};
+
+let getUVsForTriangle = (grid, a, b, c) => {
+  let textureWidth = 1 * 100;
+  let textureHeight = 0.866025 * 1 * 55;
+  console.log("getting uvs for triangle");
+  console.log({ a, b, c });
+  let ax = grid.get(a).toPoint().add(grid.get(a).center()).x;
+  let ay = grid.get(a).toPoint().add(grid.get(a).center()).y;
+  let bx = grid.get(b).toPoint().add(grid.get(b).center()).x;
+  let by = grid.get(b).toPoint().add(grid.get(b).center()).y;
+  let cx = grid.get(c).toPoint().add(grid.get(c).center()).x;
+  let cy = grid.get(c).toPoint().add(grid.get(c).center()).y;
+  let uvs = {
+    x0: ax / textureWidth,
+    y0: 1 - ay / textureHeight,
+    x1: bx / textureWidth,
+    y1: 1 - by / textureHeight,
+    x2: cx / textureWidth,
+    y2: 1 - cy / textureHeight,
+  };
+  return [uvs.x0, uvs.y0, uvs.x1, uvs.y1, uvs.x2, uvs.y2];
+};
+
+export const getIcosahedronUVs = () => {
+  const Hex = Honeycomb.extendHex({
+    size: 1, // default: 1
+    orientation: "flat", // default: 'pointy'
+  });
+  const Grid = Honeycomb.defineGrid(Hex);
+  let grid = Grid.rectangle({ width: 67, height: 28 });
+
+  let icoUVs = getGlobeTriangles()
+    .map((t) => getUVsForTriangle(grid, t[0], t[1], t[2]))
+    .flat();
+  return icoUVs;
 };
 
 export const hexTextureAsUri = (width) => {
@@ -78,7 +203,7 @@ export const hexTextureAsUri = (width) => {
   //canvas.width = width;
   //canvas.height = 0.866025 * width;
   let ctx = canvas.getContext("2d");
-  let data = drawHexTexture(ctx, canvas.width, canvas.height);
+  drawHexTexture(ctx, canvas.width, canvas.height);
   let dataURL = canvas.toDataURL("image/png");
-  return { data, dataURL };
+  return dataURL;
 };
--- js/scene.js
+++ js/scene.js
@@ -1,7 +1,9 @@
 import * as BABYLON from "babylonjs";
 import * as _ from "lodash";
 
-import { hexTextureAsUri } from "./hex.js";
+import { hexTextureAsUri, getIcosahedronUVs } from "./hex.js";
+
+//https://www.babylonjs-playground.com/#VKBJN#14
 
 export const createScene = function (engine, canvas) {
   var scene = new BABYLON.Scene(engine);
@@ -31,41 +33,22 @@ export const createScene = function (engine, canvas) {
 
   var mat = new BABYLON.StandardMaterial("mat", scene);
 
-  let { data, dataURL } = hexTextureAsUri(600);
-  console.log(data);
+  let dataURL = hexTextureAsUri(600);
 
   mat.diffuseTexture = new BABYLON.Texture(dataURL, scene);
 
-  var faceUV = new Array(20);
-
-  let uBottomLeft = 0.048;
-  let uTopRight = 0.152;
-  let vBottomLeft = 0.437;
-  let vTopRight = 0.6544627746345163;
-  let full = new BABYLON.Vector4(0, 0, 1, 1);
-  let cropped = new BABYLON.Vector4(
-    uBottomLeft,
-    vBottomLeft,
-    uTopRight,
-    vTopRight
-  );
-  let croppedFlipped = new BABYLON.Vector4(
-    uBottomLeft,
-    vTopRight,
-    uTopRight,
-    vBottomLeft
-  );
-
-  for (var i = 0; i < 20; i++) {
-    faceUV[i] = croppedFlipped;
-  }
-
   let icosahedron = BABYLON.MeshBuilder.CreatePolyhedron(
     "ico",
-    { type: 3, size: 1, faceUV },
+    { type: 3, size: 1, updatable: true },
     scene
   );
   icosahedron.material = mat;
+
+  var uvs = getIcosahedronUVs();
+
+  icosahedron.updateVerticesData(BABYLON.VertexBuffer.UVKind, uvs);
+
+  new BABYLON.AxesViewer(scene);
 
   scene.registerBeforeRender(function () {
     pl.position = camera.position;
