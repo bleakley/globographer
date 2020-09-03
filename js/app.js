52c0eb4fdc2f47d77d518340595f9a987d467bfa
Brian Bleakley <brianbleakley@gmail.com>
2020-09-03 11:00:31 -0600

    add control panel

--- js/app.js
+++ js/app.js
@@ -3,48 +3,83 @@ import { createScene } from "./scene.js";
 import { drawHexTexture } from "./hex.js";
 import { fileToData, parseWxxData } from "./load.js";
 
+let mapData = null;
+let projection = null;
+
+var canvas, input, projections;
+
+let getProjection = () => {
+  for (var i = 0, length = projections.length; i < length; i++) {
+    if (projections[i].checked) {
+      console.log(projections[i].value);
+      return projections[i].value;
+    }
+  }
+};
+
+let resetCanvas = () => {
+  var newCanvas = canvas.cloneNode(false);
+  canvas.parentNode.replaceChild(newCanvas, canvas);
+  canvas = newCanvas;
+};
+
+let renderIcosahedron = (mapData) => {
+  resetCanvas();
+  var engine = new BABYLON.Engine(canvas, true);
+  var scene = createScene(engine, canvas, mapData);
+
+  engine.runRenderLoop(function () {
+    scene.render();
+  });
+
+  window.addEventListener("resize", function () {
+    engine.resize();
+  });
+};
+
+let renderFlat = (mapData) => {
+  resetCanvas();
+  let ctx = canvas.getContext("2d");
+  canvas.width = 50 * 100;
+  canvas.height = 0.866025 * 50 * 55;
+  drawHexTexture(ctx, canvas.width, canvas.height, mapData);
+  return;
+};
+
+let render = (mapData) => {
+  if (!mapData) return;
+  switch (projection) {
+    case "flat":
+      renderFlat(mapData);
+      break;
+    case "icosahedron":
+      renderIcosahedron(mapData);
+      break;
+    default:
+      renderFlat(mapData);
+      break;
+  }
+};
+
 window.addEventListener("DOMContentLoaded", function () {
-  // get the canvas DOM element
-  var canvas = document.getElementById("renderCanvas");
-  var input = document.getElementById("fileInput");
+  canvas = document.getElementById("renderCanvas");
+  input = document.getElementById("fileInput");
+  projections = document.getElementsByName("projection");
+
+  projection = getProjection();
 
   input.onchange = async (e) => {
-    // getting a hold of the file reference
-    var file = e.target.files[0];
-
+    let file = e.target.files[0];
     let data = await fileToData(file);
-    let mapData = parseWxxData(data);
-    console.log(mapData);
-
-    /*let ctx = canvas.getContext("2d");
-    canvas.width = 50 * 100;
-    canvas.height = 0.866025 * 50 * 55;
-    drawHexTexture(ctx, canvas.width, canvas.height, mapData);
-    return;*/
-
-    // load the 3D engine
-    var engine = new BABYLON.Engine(canvas, true);
-    // call the createScene function
-    var scene = createScene(engine, canvas, mapData);
-
-    // run the render loop
-    engine.runRenderLoop(function () {
-      scene.render();
-    });
-
-    // the canvas/window resize event handler
-    window.addEventListener("resize", function () {
-      engine.resize();
-    });
-
-    // setting up the reader
-    var reader = new FileReader();
-    reader.readAsText(file, "UTF-8");
-
-    // here we tell the reader what to do when it's done reading...
-    reader.onload = (readerEvent) => {
-      var content = readerEvent.target.result; // this is the content!
-      console.log(content);
-    };
+    mapData = parseWxxData(data);
+    render(mapData);
   };
+
+  projections.forEach(
+    (item) =>
+      (item.onchange = async (e) => {
+        projection = getProjection();
+        render(mapData);
+      })
+  );
 });
